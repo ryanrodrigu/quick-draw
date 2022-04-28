3d70314e3009d1ac613f509dc99cb7f69adf290e
ryanrodrigu <97711855+ryanrodrigu@users.noreply.github.com>
2022-04-27 20:40:31 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -9,3 +9,51 @@ timer_check = "";
 drawn_sketch = "";
 answer_holder = "";
 score = 0;
+
+function updateCanvas() {
+    background("white");
+    random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+sketch = quick_draw_data_set[random_number];
+
+document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
+}
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+}
+
+function draw() {
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed)
+    {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+    check_sketch();
+    if(drawn_sketch == sketch)
+    {
+        answer_holder = "set";
+        score++;
+        document.getElementById('score').innerHTML = 'Score:' + score;
+    }
+}
+
+function check_sketch()
+{
+    timer_counter++;
+    document.getElementById('timer').innerHTML = 'Timer:'+timer_counter;
+    if(timer_counter > 600)
+    {
+        timer_counter = 0;
+        timer_check = "completed"
+    }
+    if(timer_check == "completed" || answer_holder == "set")
+    {
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }
+}
